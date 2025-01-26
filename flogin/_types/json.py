4b8f2070dba05499c2dd5250d057a46dfb919293
cibere <71997063+cibere@users.noreply.github.com>
2025-01-25 17:53:11 -0800

    fix nameerror with Jsonable

--- flogin/_types/json.py
+++ flogin/_types/json.py
@@ -1,6 +1,1 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from typing import TypeAlias
-
-Jsonable: TypeAlias = dict[str, "Jsonable"] | str | int | list["Jsonable"] | None | bool
+Jsonable = dict[str, "Jsonable"] | str | int | list["Jsonable"] | None | bool
