3a8268e2b5d9103631479c57b07f0fd992b6bfbf
cibere <71997063+cibere@users.noreply.github.com>
2025-01-25 17:51:48 -0800

    allow bools in Jsonable

--- flogin/_types/json.py
+++ flogin/_types/json.py
@@ -3,4 +3,4 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import TypeAlias
 
-Jsonable: TypeAlias = dict[str, "Jsonable"] | str | int | list["Jsonable"] | None
+Jsonable: TypeAlias = dict[str, "Jsonable"] | str | int | list["Jsonable"] | None | bool
