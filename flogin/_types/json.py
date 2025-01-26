4e889610fef74e0b45073e0acdb36c55829faa5d
cibere <71997063+cibere@users.noreply.github.com>
2025-01-25 17:48:44 -0800

    use Typealias with Jsonable

--- flogin/_types/json.py
+++ flogin/_types/json.py
@@ -1,1 +1,6 @@
-Jsonable = dict[str, "Jsonable"] | str | int | list["Jsonable"] | None
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import TypeAlias
+
+Jsonable: TypeAlias = dict[str, "Jsonable"] | str | int | list["Jsonable"] | None
